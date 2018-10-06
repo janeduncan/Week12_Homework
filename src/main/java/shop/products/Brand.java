bb0ec95ef12d7f49e95cae5a96a0c8fa3f2f58d0
janeduncan <janeduncan@live.co.uk>
2018-10-06 09:09:08 +0100

    Adds Brand ENUM

---
+++ src/main/java/shop/products/Brand.java
@@ -0,0 +1,16 @@
+package shop.products;
+
+public enum Brand {
+
+    YAMAHA,
+    VINCENTBACH,
+    TREVORJAMES,
+    ROLAND,
+    GIBSON,
+    TAMA,
+    ZILDIJAN,
+    FENDER,
+    MIYAZAWA,
+    PEARL
+
+}
